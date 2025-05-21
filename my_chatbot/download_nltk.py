39bd9ed89843c3ba22ae85490749bc3ae5e6b2e7
Dhruvil Nakrani <128470872+nd5a@users.noreply.github.com>
2025-05-21 13:06:59 +0530

    Update download_nltk.py

--- my_chatbot/download_nltk.py
+++ my_chatbot/download_nltk.py
@@ -1,2 +1,14 @@
 import nltk
-nltk.download('wordnet')
+import os
+
+nltk_data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'nltk_data')
+
+# Create directory if not exists
+os.makedirs(nltk_data_path, exist_ok=True)
+
+# Download required resources
+nltk.download('punkt', download_dir=nltk_data_path)
+nltk.download('wordnet', download_dir=nltk_data_path)
+nltk.download('omw-1.4', download_dir=nltk_data_path)
+nltk.download('averaged_perceptron_tagger', download_dir=nltk_data_path)
+nltk.download('punkt_tab', download_dir=nltk_data_path)
