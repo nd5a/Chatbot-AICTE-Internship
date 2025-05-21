baa2c39a50b7132523a3c6b89cc3ffd5570969e1
Dhruvil Nakrani <128470872+nd5a@users.noreply.github.com>
2025-05-21 15:24:45 +0530

    Update train_chatbot.py

--- my_chatbot/train_chatbot.py
+++ my_chatbot/train_chatbot.py
@@ -86,7 +86,7 @@ except Exception as e:
 
 # Build model
 model = Sequential()
-model.add(Dense(128, input_shape=(len(train_x[0]), activation='relu')))
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
